b2f8db22839c0821a61fb17288566e44293e1fd2
Justin08784 <shine777justin@gmail.com>
2023-10-05 02:56:58 -0500

    added looptests

---
+++ examples/c/looptests.bpf.c
@@ -0,0 +1,70 @@
+// looptests.bpf.c 
+
+#include <linux/bpf.h>
+#include <bpf/bpf_helpers.h>
+
+/* Globals */
+int my_pid = 0;
+
+/* Static Types/Variables */
+enum LOOP_TYPE {
+    NO_LOOP,
+    BOUNDED_LOOP,
+    INFINITE_LOOP
+};
+static int test_type = NO_LOOP; 
+
+/*
+Interestingly, an error is thrown is test_type is not static. 
+In ebpf <or just libbpf-bootstrap?>, if a variable declared in the epbf program 
+is not static, then it is a global visible to the user program.
+
+ebpf globals are added by libbpf <?> as fields to the ebpf program struct in
+the auto-generated skel.h header. (Try removing static from int test_type and
+compiling; a new "int test_type" field will appear in the struct. This is also 
+why there is a my_pid field in the struct.)
+
+I think the verifier complains if test_type is not static because this means that
+the user program, with test_type being accessible via skel->bss->test_type, might
+edit test_type to INFINITE_LOOP, which triggers an infinite loop. Simply put,
+the verifier is guarding against possible malicious behavior by the user program.
+*/
+
+
+/* SEC(...) must be done after any global declarations */
+SEC("tracepoint/syscalls/sys_enter_write") // trigger on any system write
+
+int bpf_prog(void *ctx) 
+{   
+    int pid = bpf_get_current_pid_tgid() >> 32;
+
+	if (pid != my_pid)
+        /* ignore writes not from a running looptests.c */
+		return 0;
+
+    char msg[] = "Hello, World!";
+
+    switch (test_type) {
+        case NO_LOOP: {
+            /* should pass */
+            bpf_printk("invoke bpf_prog: %s\n", msg);
+            return 0;
+        }
+        case BOUNDED_LOOP: {
+            /* should pass */
+            for (int i = 0; i < 4; i++)
+                bpf_printk("invoke bpf_prog: %s\n", msg);
+            return 0;
+        }
+        case INFINITE_LOOP: {
+            /* should fail */
+            for (;;)
+                bpf_printk("invoke bpf_prog: %s\n", msg);
+            return 0;
+        }
+    }
+
+    return 0;
+}
+
+char LICENSE[] SEC("license") = "Dual BSD/GPL";
