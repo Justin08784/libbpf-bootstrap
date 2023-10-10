f4af3c274f4e22a77566e99906f6833cfd324e3a
Justin08784 <shine777justin@gmail.com>
2023-10-10 05:26:22 -0500

    added custom test cases to looptests

--- examples/c/looptests.bpf.c
+++ examples/c/looptests.bpf.c
@@ -10,7 +10,11 @@ int my_pid = 0;
 enum LOOP_TYPE {
     NO_LOOP,
     BOUNDED_LOOP,
-    INFINITE_LOOP
+    INFINITE_LOOP,
+    CUSTOM1,
+    CUSTOM2,
+    CUSTOM3, 
+    CUSTOM4
 };
 static int test_type = NO_LOOP; 
 
@@ -33,6 +37,43 @@ the verifier is guarding against possible malicious behavior by the user program
 
 /* SEC(...) must be done after any global declarations */
 SEC("tracepoint/syscalls/sys_enter_write") // trigger on any system write
+
+void custom1_infinite_loop()
+{
+    for (int i = 0; i < i + 1; i++) {}
+}
+
+void custom2_infinite_loop()
+{
+    for (int i = 0; i < 5;) {}
+}
+
+void custom3_bounded_loop()
+{
+    /* An attempt of using a condition based on arithmetic
+    to confuse the verifier into thinking an infinite loop may execute. 
+    
+    Tests if the verifier is conservative and bans anything that seems 
+    suspiciously loopy. */
+    int x = 3;
+    int y = x + x - x;
+    y = ((x + 1) * (x + 1) - x * x - 1) / 2;
+    // find some computationally expensive operation to set y
+    if (x == y) {
+        return;
+    } else {
+        // should never execute
+        for (;;) {}
+    }
+    return;
+}
+
+void custom4_empty_body_loop()
+{
+    /* An infinite loop with an empty body. This executes without problem,
+    strangely. */
+    for (;;) {}
+}
 
 int bpf_prog(void *ctx) 
 {   
@@ -62,6 +103,18 @@ int bpf_prog(void *ctx)
                 bpf_printk("invoke bpf_prog: %s\n", msg);
             return 0;
         }
+        case CUSTOM1:
+            custom1_infinite_loop();
+            break;
+        case CUSTOM2:
+            custom2_infinite_loop();
+            break;
+        case CUSTOM3:
+            custom3_bounded_loop();
+            break;
+        case CUSTOM4:
+            custom4_empty_body_loop();
+            break;
     }
 
     return 0;
